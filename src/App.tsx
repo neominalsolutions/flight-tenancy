e6e0077164d25a9f80e6ecb5bb86ea6a900789bb
neominalsolutions <neominal.solutions@gmail.com>
2024-03-19 18:39:24 +0300

    Dynamic Route From Json

--- src/App.tsx
+++ src/App.tsx
@@ -11,60 +11,69 @@ import ArrivalDetailPage from './pages/arrivals/ArrivalDetailPage';
 import DepartureDetailPage from './pages/departures/DepartureDetailPage';
 
 import menus from './menu.json';
-import { ComponentRegister } from './layouts/ComponentRegisteration';
+import { ComponentRegistry } from './layouts/ComponentRegisteration';
 
 function App() {
-	// (menus as any[]).map((menu) => {
-	// 	return {
-	// 		path: menu.path,
-	// 		Component: ComponentRegister?[menu?.layoutName]
-	// 	};
-	// });
+	const _routes = (menus as any[]).map((menu: any) => {
+		return {
+			path: menu.path,
+			Component: ComponentRegistry[menu.component],
+			children: menu.children.map((child: any) => {
+				return {
+					path: child.path,
+					Component: ComponentRegistry[child.component],
+					children: [],
+				} as RouteObject;
+			}),
+		} as RouteObject;
+	});
 
-	return useRoutes([
-		{
-			path: '',
-			Component: MainLayout,
-			children: [
-				{
-					path: 'arrivals',
-					Component: ArrivalsPage,
-				},
-				{
-					path: 'departures',
-					Component: DeparturesPage,
-				},
-			],
-		},
-		{
-			path: 'thy', // thy/arrivals/tk-300
-			Component: ThyLayout,
-			children: [
-				{
-					path: 'arrivals/:flightNumber',
-					Component: ArrivalDetailPage,
-				},
-				{
-					path: 'departures/:flightNumber',
-					Component: DeparturesPage,
-				},
-			],
-		},
-		{
-			path: 'anadolu-jet',
-			Component: AnadoluJetLayout,
-			children: [
-				{
-					path: 'arrivals/:flightNumber',
-					Component: ArrivalDetailPage,
-				},
-				{
-					path: 'departures/:flightNumber',
-					Component: DepartureDetailPage,
-				},
-			],
-		},
-	]);
+	return useRoutes(_routes);
+
+	// return useRoutes([
+	// 	{
+	// 		path: '',
+	// 		Component: MainLayout,
+	// 		children: [
+	// 			{
+	// 				path: 'arrivals',
+	// 				Component: ArrivalsPage,
+	// 			},
+	// 			{
+	// 				path: 'departures',
+	// 				Component: DeparturesPage,
+	// 			},
+	// 		],
+	// 	},
+	// 	{
+	// 		path: 'thy', // thy/arrivals/tk-300
+	// 		Component: ThyLayout,
+	// 		children: [
+	// 			{
+	// 				path: 'arrivals/:flightNumber',
+	// 				Component: ArrivalDetailPage,
+	// 			},
+	// 			{
+	// 				path: 'departures/:flightNumber',
+	// 				Component: DepartureDetailPage,
+	// 			},
+	// 		],
+	// 	},
+	// 	{
+	// 		path: 'anadolu-jet',
+	// 		Component: AnadoluJetLayout,
+	// 		children: [
+	// 			{
+	// 				path: 'arrivals/:flightNumber',
+	// 				Component: ArrivalDetailPage,
+	// 			},
+	// 			{
+	// 				path: 'departures/:flightNumber',
+	// 				Component: DepartureDetailPage,
+	// 			},
+	// 		],
+	// 	},
+	// ]);
 }
 
 export default App;
--- src/layouts/ComponentRegisteration.ts
+++ src/layouts/ComponentRegisteration.ts
@@ -1,9 +1,17 @@
+import ArrivalDetailPage from '../pages/arrivals/ArrivalDetailPage';
+import ArrivalsPage from '../pages/arrivals/ArrivalsPage';
+import DepartureDetailPage from '../pages/departures/DepartureDetailPage';
+import DeparturesPage from '../pages/departures/DeparturesPage';
 import AnadoluJetLayout from './AnadoluJetLayout';
 import MainLayout from './MainLayout';
 import ThyLayout from './ThyLayout';
 
-export const ComponentRegister = {
+export const ComponentRegistry: any = {
 	MainLayout: MainLayout,
 	ThyLayout: ThyLayout,
+	ArrivalsPage: ArrivalsPage,
+	DeparturesPage: DeparturesPage,
+	ArrivalDetailPage: ArrivalDetailPage,
+	DepartureDetailPage: DepartureDetailPage,
 	AnadoluJetLayout: AnadoluJetLayout,
 };
